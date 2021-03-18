ea2b50c006b20279b7909f5133c9ef047d76b4be
CarinaSt <carina.steindl@edu.fh-joanneum.at>
2021-03-18 12:19:23 +0100

    ready to copy

--- Assets/Desertballoons_Kate&Carina/Scripts/FuggooController.cs
+++ Assets/Desertballoons_Kate&Carina/Scripts/FuggooController.cs
@@ -1,24 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FuggooController : MonoBehaviour
 {
-   public GameObject explosion; // drag explosion prefab here
-                                // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        void OnCollisionEnter()
-        {
-            GameObject collisionobject = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
-            Destroy(gameObject); // destroy the balloon
-            Destroy(collisionobject, 3); // delete the explosion after 3 seconds
-        }
+        Destroy(other.gameObject);
     }
 }
--- Assets/Desertballoons_Kate&Carina/Scripts/MainCameraController.cs
+++ Assets/Desertballoons_Kate&Carina/Scripts/MainCameraController.cs
@@ -7,7 +7,7 @@ public class MainCameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,9 +15,12 @@ public class MainCameraController : MonoBehaviour
     {
         RaycastHit hit;     //variable
         var ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition); // we create a ray
-        if(Physics.Raycast(ray, out hit, 50) && (hit.rigidbody != null));       //put it into information with physics and give information
-            if(hit.collider.tag == "jumpy") {
-            hit.rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
-           } 
-       }
+        if (Physics.Raycast(ray, out hit, 50) && (hit.rigidbody != null))
+        {       //put it into information with physics and give information
+            if (hit.collider.tag == "jumpy")
+            {
+                hit.rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
+            }
+        }
+    }
 }
