e7508fa6957954e3115dfa5a280bdd83ec738d30
CarinaSt <carina.steindl@edu.fh-joanneum.at>
2021-11-19 14:17:17 +0100

    different interaction methode

--- Assets/Desertballoons_Kate&Carina/Scripts/MainCameraController.cs
+++ Assets/Desertballoons_Kate&Carina/Scripts/MainCameraController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MainCameraController : MonoBehaviour
 {
+    public Transform centerObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +16,26 @@ public class MainCameraController : MonoBehaviour
     {
         RaycastHit hit;     //variable
         var ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition); // we create a ray
-        if (Physics.Raycast(ray, out hit, 50) && (hit.rigidbody != null))
+        if (Physics.Raycast(ray, out hit, 50000))
         {       //put it into information with physics and give information
-            if (hit.collider.tag == "jumpy")
-            {
+            //if (hit.collider.tag == "jumpy")
+            //{
                 //hit.rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
-                hit.rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
+               // hit.rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
+            //}
+            if (hit.collider.tag == "floor")
+            {
+                foreach(GameObject balloon in GameObject.FindGameObjectsWithTag("jumpy"))
+                {
+                    Debug.Log("huhu");
+                    Vector3 direction = hit.point - centerObject.position;
+                    balloon.GetComponent<Rigidbody>().AddForce(direction*Time.deltaTime*15, ForceMode.Force);
+                }
+               
+               
+
+    
+
             }
         }
     }
